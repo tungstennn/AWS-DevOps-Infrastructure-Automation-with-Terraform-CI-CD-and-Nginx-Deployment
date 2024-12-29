c62f688469bc4b32e57fc49c81e02725b02c1d1b
tungstennn <aqorane77@gmail.com>
2024-12-29 22:02:38 +0000

    Update stage environment

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -1,3 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  required_version = ">= 1.5.0"
+}
+
+provider "aws" {
+  region = var.region
+}
+
 module "vpc" {
   source                  = "../../modules/vpc"
   cidr_block              = var.cidr_block

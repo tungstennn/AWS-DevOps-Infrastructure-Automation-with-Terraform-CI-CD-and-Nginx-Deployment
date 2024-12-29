3deb4f9e90c49d1476047083c05eb3b0cb670122
tungstennn <aqorane77@gmail.com>
2024-12-29 21:01:44 +0000

    Update dev environment

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,3 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  required_version = ">= 1.5.0"
+}
+
+provider "aws" {
+  region = var.region
+}
+
 module "vpc" {
   source                  = "../../modules/vpc"
   cidr_block              = var.cidr_block
@@ -13,7 +28,7 @@ module "security_group" {
   vpc_id         = module.vpc.vpc_id
   name           = "${var.environment}-web-sg"
   allowed_ssh_ip = var.allowed_ssh_ip
-  
+
 }
 
 module "ec2" {
--- global/providers.tf
+++ global/providers.tf
@@ -11,5 +11,4 @@ terraform {
 
 provider "aws" {
   region = var.region
-  #profile = var.aws_profile
 }
